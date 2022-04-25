69abc836443e9b37a446c92ca20251d1deca94a7
Chitsanupong <c.tangvasinkul@gmail.com>
2022-04-25 22:47:12 +0700

    feat: add html template

--- main.go
+++ main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home page")
+	renderTemplate(w, "home.html")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About page")
+}
+
+func renderTemplate(w http.ResponseWriter, name string) {
+	parsedTemplate, _ := template.ParseFiles("templates/" + name)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
 }
 
 func main() {
